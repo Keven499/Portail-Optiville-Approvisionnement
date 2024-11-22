2e0106aa2eadba0cb698086ed7575e3de9bbd317
MariusTestard <tehtar4857@gmail.com>
2024-11-22 13:57:37 -0500

    Update ProduitServiceService.cs
    J'ai implémenter la feature de l'historique pour les produits et services. Il reste contacts.

--- Data/Services/ProduitServiceService.cs
+++ Data/Services/ProduitServiceService.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Portail_OptiVille.Data.FormModels;
 using Portail_OptiVille.Data.Models;
+using Newtonsoft.Json;
 
 namespace Portail_OptiVille.Data.Services
 {
     public class ProduitServiceService
     {
         private readonly A2024420517riGr1Eq6Context _context;
+        private HistoriqueService _historiqueService;
 
-        public ProduitServiceService(A2024420517riGr1Eq6Context context)
+        public ProduitServiceService(A2024420517riGr1Eq6Context context, HistoriqueService historiqueService)
         {
             _context = context;
+            _historiqueService = historiqueService;
         }
 
         public async Task SaveProduitServiceData(ProduitServiceFormModel produitServiceFormModelDto)
@@ -48,33 +51,56 @@ namespace Portail_OptiVille.Data.Services
             }
         }
 
-        public async Task UpdateProduitServiceData(ProduitServiceFormModel produitServiceFormModel, int idFournisseur)
+        public async Task UpdateProduitServiceData(ProduitServiceFormModel produitServiceFormModel, int idFournisseur, string email)
         {
+            bool isEqual = true;
             var fournisseur = await _context.Fournisseurs.Include(f => f.IdProduitServices).FirstOrDefaultAsync(f => f.IdFournisseur == idFournisseur);
-
             if (fournisseur == null)
             {
                 throw new Exception("Fournisseur non trouvé.");
             }
-
             try
             {
+                string oldData = fournisseur.DetailSpecification;
+                string newData = produitServiceFormModel.Message;
+                string[] keyData = {"Détails et spécifications", "Produits et services"};
+                var oldDict = new Dictionary<string, object> { { "Section", "Produits et services" } };
+                var newDict = new Dictionary<string, object> { { "Section", "Produits et services" } };
+                if (!oldData.Equals(newData))
+                {
+                    isEqual = false;
+                    oldDict.Add(keyData[0], oldData);
+                    newDict.Add(keyData[0], newData);
+                }
                 fournisseur.DetailSpecification = produitServiceFormModel.Message;
 
                 var selectedProduitServiceIds = produitServiceFormModel.SousProduitSelected.Where(x => x.Value).Select(x => x.Key).ToList();
                 var existingProduitServiceIds = fournisseur.IdProduitServices.Select(ps => ps.CodeUnspsc).ToList();
                 var produitServicesToAdd = await _context.Produitservices.Where(ps => selectedProduitServiceIds.Contains(ps.CodeUnspsc) && !existingProduitServiceIds.Contains(ps.CodeUnspsc)).ToListAsync();
                 var produitServicesToRemove = fournisseur.IdProduitServices.Where(ps => !selectedProduitServiceIds.Contains(ps.CodeUnspsc)).ToList();
+                List<string> produitToAdd = new List<string>();
+                List<string> produitToRemove = new List<string>();
 
                 foreach (var produitService in produitServicesToAdd)
                 {
+                    isEqual = false;
                     fournisseur.IdProduitServices.Add(produitService);
+                    produitToAdd.Add(produitService.CodeUnspsc + " - " + produitService.Description);
                 }
+                newDict.Add(keyData[1], string.Join(": ", produitToAdd));
 
                 foreach (var produitService in produitServicesToRemove)
                 {
+                    isEqual = false;
                     fournisseur.IdProduitServices.Remove(produitService);
+                    produitToRemove.Add(produitService.CodeUnspsc + " - " + produitService.Description);
                 }
+                oldDict.Add(keyData[1], string.Join(": ", produitToRemove));
+
+                string oldJSON = JsonConvert.SerializeObject(oldDict, Formatting.None);
+                string newJSON = JsonConvert.SerializeObject(newDict, Formatting.None);
+                if (!isEqual)
+                    await _historiqueService.ModifyEtat("Modifiée", fournisseur.IdFournisseur, email, null, oldJSON, newJSON);
 
                 await _context.SaveChangesAsync();
             }
