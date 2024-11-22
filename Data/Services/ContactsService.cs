1c57675f6e961d24a18261885b07f78daed362db
MariusTestard <tehtar4857@gmail.com>
2024-11-22 18:33:29 -0500

    Update ContactService.cs
    Implémentation d'historique pour contact terminer. Il reste juste à gérer l'affichage dans HistoriqueFiche et peut-être optimiser mieux le service contact.

--- Data/Services/ContactsService.cs
+++ Data/Services/ContactsService.cs
@@ -2,19 +2,18 @@
 using Portail_OptiVille.Data.FormModels;
 using Portail_OptiVille.Data.Models;
 using Newtonsoft.Json;
-using System.Diagnostics;
 
 namespace Portail_OptiVille.Data.Services
 {
     public class ContactsService
     {
         private readonly A2024420517riGr1Eq6Context _context;
-        private HistoriqueService _historiqueFiche;
+        private HistoriqueService _historiqueService;
 
-        public ContactsService(A2024420517riGr1Eq6Context context, HistoriqueService historiqueFiche)
+        public ContactsService(A2024420517riGr1Eq6Context context, HistoriqueService historiqueService)
         {
             _context = context;
-            _historiqueFiche = historiqueFiche;
+            _historiqueService = historiqueService;
         }
 
         public async Task SaveContactsData(ContactHosterFormModel contactHosterFormModelDto)
@@ -28,6 +27,11 @@ namespace Portail_OptiVille.Data.Services
 
         public async Task UpdateContactsData(ContactHosterFormModel contactHosterFormModel, int idFournisseur, string email)
         {
+            bool isEqual = true;
+            string[] keyData = {"Prénom", "Nom", "Fonction", 
+                                "Adresse courriel", "Type de numéro", "Numéro de téléphone", "Poste"};
+            var oldDict = new Dictionary<string, object> { { "Section", "Contacts" } };
+            var newDict = new Dictionary<string, object> { { "Section", "Contacts" } };
             /*  USER CAN DO THE FOLLOWING ACTIONS:
                 - MODIFY (Add & Remove)
                 - ADD (Add)
@@ -35,16 +39,45 @@ namespace Portail_OptiVille.Data.Services
                 ALL THESE ACTIONS NEEDS TO BE INSERTED IN THE TABLE HISTORIQUE IN ONE ADD
             */
 
-            /*  COUNT ALL THE CONTACTS IN THE TABLE FOR A GIVEN IDFOURNISSEUR 
-            - REMOVE (Remove)
-            */
-            List<Contact> contacts = await _context.Contacts.Where(c => c.Fournisseur == idFournisseur).ToListAsync();
-            List<ContactFormModel> contactHosterFormModelContacts = contactHosterFormModel.ContactList;
-            var missingContacts = contacts.Where(c => !contactHosterFormModelContacts
-                .Any(modelContact => modelContact.IdContact == c.IdContact)).ToList();
+            // - REMOVE (Remove)
+            List<Contact> oldDataContacts = await _context.Contacts.Where(c => c.Fournisseur == idFournisseur).ToListAsync();
+            List<ContactFormModel> newDataContacts = contactHosterFormModel.ContactList;
+
+            // I NEED A LIST OF STRING FOR EACH KEY BECAUSE THE USER COULD REMOVE MORE THEN 1 CONTACT IN A SINGLE MODIFICATION
+            List<string> catToAddPrenom = new List<string>();
+            List<string> catToAddNom = new List<string>();
+            List<string> catToAddFonction = new List<string>();
+            List<string> catToAddAdresseCourriel = new List<string>();
+            List<string> catToAddTypeNumeroTel = new List<string>();
+            List<string> catToAddNumeroTel = new List<string>();
+            List<string> catToAddPoste = new List<string>();
+
+            List<string> catToRemovePrenom = new List<string>();
+            List<string> catToRemoveNom = new List<string>();
+            List<string> catToRemoveFonction = new List<string>();
+            List<string> catToRemoveAdresseCourriel = new List<string>();
+            List<string> catToRemoveTypeNumeroTel = new List<string>();
+            List<string> catToRemoveNumeroTel = new List<string>();
+            List<string> catToRemovePoste = new List<string>();
+
+            List<Contact> missingContacts = oldDataContacts
+                .Where(c => !newDataContacts
+                    .Any(modelContact => modelContact.IdContact == c.IdContact))
+                .ToList();
             foreach (var missingContact in missingContacts)
             {
+                var phone = await _context.Telephones
+                    .Where(t => t.Contact == missingContact.IdContact)
+                    .FirstOrDefaultAsync();
+                isEqual = false;
                 _context.Contacts.Remove(missingContact);
+                catToRemovePrenom.Add(missingContact.Prenom);
+                catToRemoveNom.Add(missingContact.Nom);
+                catToRemoveFonction.Add(missingContact.Fonction);
+                catToRemoveAdresseCourriel.Add(missingContact.AdresseCourriel);
+                catToRemoveTypeNumeroTel.Add(phone.Type);
+                catToRemoveNumeroTel.Add(phone.NumTelephone);
+                catToRemovePoste.Add(phone.Poste);
             }
             try
             {
@@ -58,10 +91,18 @@ namespace Portail_OptiVille.Data.Services
             //- MODIFY (Add & Remove)
             foreach (var contactFromList in contactHosterFormModel.ContactList)
             {
+                isEqual = false;
                 var existingContact = await _context.Contacts.SingleOrDefaultAsync(c => c.IdContact == contactFromList.IdContact);
                 if (existingContact != null)
                 {
-                    existingContact.IdContact = contactFromList.IdContact;
+                    if (existingContact.Prenom != contactFromList.Prenom) { catToAddPrenom.Add(contactFromList.Prenom);
+                                                                            catToRemovePrenom.Add(existingContact.Prenom); }
+                    if (existingContact.Nom != contactFromList.Nom) { catToAddNom.Add(contactFromList.Nom);
+                                                                        catToRemoveNom.Add(existingContact.Nom); }
+                    if (existingContact.Fonction != contactFromList.Fonction) { catToAddFonction.Add(contactFromList.Fonction);
+                                                                                catToRemoveFonction.Add(existingContact.Fonction); }
+                    if (existingContact.AdresseCourriel != contactFromList.AdresseCourriel) { catToAddAdresseCourriel.Add(contactFromList.AdresseCourriel);
+                                                                                                catToRemoveAdresseCourriel.Add(existingContact.AdresseCourriel); }
                     existingContact.Prenom = contactFromList.Prenom;
                     existingContact.Nom = contactFromList.Nom;
                     existingContact.Fonction = contactFromList.Fonction;
@@ -79,7 +120,12 @@ namespace Portail_OptiVille.Data.Services
                     var existingTelephone = await _context.Telephones.SingleOrDefaultAsync(t => t.Contact == existingContact.IdContact);
                     if (existingTelephone != null)
                     {
-                        existingTelephone.Contact = contactFromList.IdContact;
+                        if (existingTelephone.Type != contactFromList.TypeTelephone) { catToAddTypeNumeroTel.Add(contactFromList.TypeTelephone);
+                                                                                                catToRemoveTypeNumeroTel.Add(existingTelephone.Type); }
+                        if (existingTelephone.NumTelephone != contactFromList.Telephone) { catToAddNumeroTel.Add(contactFromList.Telephone);
+                                                                                        catToRemoveNumeroTel.Add(existingTelephone.NumTelephone); }    
+                        if (existingTelephone.Poste != contactFromList.Poste) { catToAddPoste.Add(contactFromList.Poste);
+                                                                                catToRemovePoste.Add(existingTelephone.Poste); }                                                                   
                         existingTelephone.Type = contactFromList.TypeTelephone;
                         existingTelephone.NumTelephone = contactFromList.Telephone;
                         existingTelephone.Poste = contactFromList.Poste;   
@@ -98,9 +144,73 @@ namespace Portail_OptiVille.Data.Services
                 {
                     // CREATES THE NEW CONTACT IF NOT IN THE DATABASE MEANING IT'S A NEW CONTACT FROM THE MODIFICATION
                     // ADD (Add)
-                    await AddContact(contactFromList, idFournisseur);
+                    isEqual = false;
+                    var contact = new Contact
+                    {
+                        Prenom = contactFromList.Prenom,
+                        Nom = contactFromList.Nom,
+                        Fonction = contactFromList.Fonction,
+                        AdresseCourriel = contactFromList.AdresseCourriel,
+                        Fournisseur = idFournisseur 
+                    };
+
+                    try
+                    {
+                        _context.Contacts.Add(contact);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("Une erreur est survenue lors de la sauvegarde du contact", ex);
+                    }
+
+                    var lastContactId = await _context.Contacts.MaxAsync(f => (int?)f.IdContact);
+                    var telephone = new Telephone
+                    {
+                        Type = contactFromList.TypeTelephone,
+                        NumTelephone = contactFromList.Telephone,
+                        Poste = contactFromList.Poste,
+                        Contact = lastContactId,
+                        Coordonnee = null
+                    };
+
+                    catToAddPrenom.Add(contact.Prenom);
+                    catToAddNom.Add(contact.Nom);
+                    catToAddFonction.Add(contact.Fonction);
+                    catToAddAdresseCourriel.Add(contact.AdresseCourriel);
+                    catToAddTypeNumeroTel.Add(telephone.Type);
+                    catToAddNumeroTel.Add(telephone.NumTelephone);
+                    catToAddPoste.Add(telephone.Poste);
+
+                    try
+                    {
+                        _context.Telephones.Add(telephone);
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("Une erreur est survenue lors de la sauvegarde du téléphone", ex);
+                    }
                 }
             }
+            oldDict.Add(keyData[0], string.Join(": ", catToRemovePrenom));
+            oldDict.Add(keyData[1], string.Join(": ", catToRemoveNom));
+            oldDict.Add(keyData[2], string.Join(": ", catToRemoveFonction));
+            oldDict.Add(keyData[3], string.Join(": ", catToRemoveAdresseCourriel));
+            oldDict.Add(keyData[4], string.Join(": ", catToRemoveTypeNumeroTel));
+            oldDict.Add(keyData[5], string.Join(": ", catToRemoveNumeroTel));
+            oldDict.Add(keyData[6], string.Join(": ", catToRemovePoste));
+            newDict.Add(keyData[0], string.Join(": ", catToAddPrenom));
+            newDict.Add(keyData[1], string.Join(": ", catToAddNom));
+            newDict.Add(keyData[2], string.Join(": ", catToAddFonction));
+            newDict.Add(keyData[3], string.Join(": ", catToAddAdresseCourriel));
+            newDict.Add(keyData[4], string.Join(": ", catToAddTypeNumeroTel));
+            newDict.Add(keyData[5], string.Join(": ", catToAddNumeroTel));
+            newDict.Add(keyData[6], string.Join(": ", catToAddPoste));
+            string oldJSON = JsonConvert.SerializeObject(oldDict, Formatting.None);
+            string newJSON = JsonConvert.SerializeObject(newDict, Formatting.None);
+            if (!isEqual)
+                await _historiqueService.ModifyEtat("Modifiée", idFournisseur, email, null, oldJSON, newJSON);
         }
 
         public async Task AddContact(ContactFormModel contactFromList, int idFournisseur)
