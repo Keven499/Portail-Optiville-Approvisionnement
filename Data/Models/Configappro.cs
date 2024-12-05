faa0a8ac3dae7adcef7b4ccb59ed3582daa72731
Dahlias <gpoisson0918@gmail.com>
2024-12-05 16:53:41 -0500

    push

--- Data/Models/Configappro.cs
+++ Data/Models/Configappro.cs
@@ -14,4 +14,6 @@ public partial class Configappro
     public int TailleMaxFichiers { get; set; }
 
     public string CourrielFinance { get; set; } = null!;
+
+    public int LimiteMaxFichiers { get; set; }
 }
