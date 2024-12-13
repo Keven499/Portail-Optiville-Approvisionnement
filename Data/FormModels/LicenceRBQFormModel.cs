23728acc3c79457ad69eea9d805e79350d8b25a4
MariusTestard <tehtar4857@gmail.com>
2024-12-13 14:47:19 -0500

    Update LicenceRBQFormModel
    Plus besoin d'utiliser le champ disabled dans le model.

--- Data/FormModels/LicenceRBQFormModel.cs
+++ Data/FormModels/LicenceRBQFormModel.cs
@@ -17,8 +17,6 @@ namespace Portail_OptiVille.Data.FormModels
         [NothingSelected(ErrorMessage = "Aucune case n'a été coché")]
         public Dictionary<string, bool> SousCategoSelected { get; set; } = new Dictionary<string, bool>();
 
-        public bool Disabled = false;
-
         public List<string> CodeSousCategorie { get; set; } = new List<string>();
     }
 }
